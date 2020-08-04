60858926088b718932e39cad830fa0372079fdc9
git-austinmccollom <austinmccollom@gmail.com>
2020-08-04 16:33:45 -0500

    able to open placeholder div when double clicking images, will switch to showing the picture next

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
 // Your code goes here
 const navNodes = document.querySelectorAll('a');
+const containerHome = document.querySelector('.container.home');
+const body = document.querySelector('body');
 
 // navNodes.addEventListener('mouseover', event => {
 //     console.log('hello world');
@@ -11,5 +13,16 @@ Array.from(navNodes).forEach(element => {
     })
     element.addEventListener('mouseleave', event => {
         element.style.backgroundColor = 'white';
+    })
+})
+
+const images = document.querySelectorAll('img');
+
+Array.from(images).forEach(element => {
+    element.addEventListener('dblclick', event => {
+        console.log('hello?');
+        let newDiv = document.createElement('div');
+        newDiv.style.cssText = "width: 75%; height: 600px; background-color: red; margin: auto;"
+        body.insertBefore(newDiv, containerHome);
     })
 })
