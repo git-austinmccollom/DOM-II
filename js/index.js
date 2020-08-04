85d1117db8102a12549ac55a480816aa4eeedb69
git-austinmccollom <austinmccollom@gmail.com>
2020-08-04 16:09:49 -0500

    mouseenter and mouseleave events animated

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
 // Your code goes here
+const navNodes = document.querySelectorAll('a');
+
+// navNodes.addEventListener('mouseover', event => {
+//     console.log('hello world');
+// })
+
+Array.from(navNodes).forEach(element => {
+    element.addEventListener('mouseenter', event => {
+        element.style.backgroundColor = 'green';
+    })
+    element.addEventListener('mouseleave', event => {
+        element.style.backgroundColor = 'white';
+    })
+})
